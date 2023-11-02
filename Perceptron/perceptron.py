9a2e6df4e7fd06805fc2b41709d2ea25db746a1c
Kutay Eken <kutayeken1@gmail.com>
2023-11-02 13:07:43 -0600

    implemented average perceptron

--- Perceptron/perceptron.py
+++ Perceptron/perceptron.py
@@ -32,6 +32,20 @@ def standard(x_data,y_data,epoch):
                 w = w + y_i * row_list        
     return w
 
+def averaged(x_data,y_data,epoch):
+    w = np.zeros(len(x_data.columns))
+    a = np.zeros(len(x_data.columns))
+    for i in range(epoch):
+        for index,row in x_data.iterrows():
+            row_list = row.to_numpy()
+            y_i = y_data[index]
+            val = y_i * np.dot(w.transpose(),row_list)
+            if val <= 0:
+                w = w + y_i * row_list 
+            a = a + w 
+    return a,w 
+
+
 def predict(x_data,w):
     y_pred = []
     for index, row in x_data.iterrows():
@@ -41,10 +55,22 @@ def predict(x_data,w):
 
 
 train,test,y_train,y_test = set_data()
+
 w_std = (standard(train,y_train,10))
 pred_std = np.array(predict(test,w_std))
 similarity_std = np.sum(y_test == pred_std) / len(y_test)
 print("----------STANDARD PERCEPTRON RESULTS:----------")
 print("LEARNED WEIGHT VECTOR: ", w_std)
 print("AVERAGE PREDICTION ERROR ON TEST DATASET: ",1 - similarity_std)
-print("PERCENTAGE ERROR: ",(1 - similarity_std) * 100)
+print("ERROR PERCENTAGE: ",(1 - similarity_std) * 100)
+print("------------------------------------------------")
+
+
+a_ave,w_ave = averaged(train,y_train,10)
+pred_ave = np.array(predict(test,a_ave))
+similarity_ave = np.sum(y_test == pred_ave) / len(y_test)
+print("----------AVERAGED PERCEPTRON RESULTS:----------")
+print("LEARNED WEIGHT VECTOR: ", w_ave)
+print("AVERAGE PREDICTION ERROR ON TEST DATASET: ",1 - similarity_ave)
+print("ERROR PERCENTAGE: ",(1 - similarity_ave) * 100)
+print("------------------------------------------------")
